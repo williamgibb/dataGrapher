f5701de7cffdb42c9abf731ea6c80490d2466854
William Gibb <williamgibb@gmail.com>
2016-04-20 21:55:20 -0400

    If there is no func to execute, print help and exit.

--- app/__main__.py
+++ app/__main__.py
@@ -256,6 +256,9 @@ if __name__ == '__main__':
                         level=logging.DEBUG, )
     parser = get_parser()
     opts = parser.parse_args()
+    if 'func' not in opts:
+        parser.print_help()
+        sys.exit(1)
     if not opts.verbose:
         logging.disable(logging.DEBUG)
     opts.func(opts)
