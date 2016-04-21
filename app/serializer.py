046024fd48c2a01a7b71ca2ab21803b4a2cd220c
William Gibb <williamgibb@gmail.com>
2016-04-20 21:17:36 -0400

    Remove unused code.

--- app/serializer.py
+++ app/serializer.py
@@ -9,33 +9,6 @@ from . import utils
 log = logging.getLogger(__name__)
 
 
-class MockSerializer(threading.Thread):
-    # noinspection PyUnusedLocal
-    def __init__(self,
-                 output_queue: multiprocessing.Queue,
-                 die_event: multiprocessing.Event,
-                 serial_lock: multiprocessing.Lock,
-                 **kwargs
-                 ):
-        super().__init__()
-        self.queue = output_queue
-        self.die_event = die_event
-        self.lock = serial_lock
-
-    def run(self):
-        log.info('{} is running!'.format(self.name))
-        while True:
-            if self.die_event.is_set():
-                break
-            try:
-                v = self.queue.get(timeout=1)
-            except queue.Empty:
-                continue
-            with self.lock:
-                log.info("Read: {}".format(v))
-        log.info('[{}] is exiting'.format(self.name))
-
-
 class DBSerializer(threading.Thread):
     # noinspection PyUnusedLocal
     def __init__(self,
